0d7c269d694b0f2a43ccc5a4d572592703944942
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-03-28 16:36:56 +0200

    Update 22
    ^ Moved to Deployer
    ^ Changed OAuth to Heroku

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,6 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
