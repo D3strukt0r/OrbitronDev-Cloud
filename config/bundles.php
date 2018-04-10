c93f18dac3c967d77478131cda247ba231cc79ce
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-04-10 13:37:45 +0200

    Update 28
    + Added cs-fixer/security-checker/phpunit

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
